98bf711ab4695872398e6d16f5615090abc4a08d
Bronson Brown <Brsn90@icloud.com>
2020-02-29 20:45:41 -0700

    2/29/2020

--- config/key.js
+++ config/key.js
@@ -1,3 +1,3 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://admin:MJC3vhvKXTaWEpUv@helioatlas-1vrvg.mongodb.net/users?retryWrites=true&w=majority'
-};
+
+
+export const mongoURI = 'mongodb+srv://admin:MJC3vhvKXTaWEpUv@helioatlas-1vrvg.mongodb.net/account?retryWrites=true&w=majority';
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
 const app = express();
-
-//Bodyparser middleware
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+// Bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
 app.use(bodyParser.json());
-
 //DB Config
 const db = require("./config/keys").mongoURI;
 
