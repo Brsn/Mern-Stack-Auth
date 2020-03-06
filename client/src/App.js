9296f5825a47027c7cfa066e211ce41cf371c943
Bronson Brown <Brsn90@icloud.com>
2020-03-05 19:41:25 -0700

    3/5/2020

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
 
 import Navbar from "./Components/layout/Navbar";
 import Landing from "./Components/layout/Landing";
+import Register from "./Components/auth/Register";
+import Login from "./Components/auth/Login";
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
     );
   }
 }
